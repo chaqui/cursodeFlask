f06e2ad5387d8fd78fada71391bc0738098c3123
Josue Chaqui <josue.chaqui@gmail.com>
2019-06-13 20:53:50 -0600

    session en flask

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask,request, make_response, redirect, render_template,session
+from flask_bootstrap import Bootstrap
+
+
+
+app = Flask(__name__)
+
+bootstrap = Bootstrap(app)
+
+app.config['SECRET_KEY'] = 'SUPER SECRETO'
+todos = ['todo 1', 'todo2', 'todo3']
+
+@app.errorhandler(404)
+def not_found(err):
+  return "error "+str(err)
+
+@app.route('/')
+def index():
+  user_ip = request.remote_addr
+  response = make_response(redirect('/hello'))
+  session['user_ip'] =user_ip
+  return response
+
+
+@app.route('/hello')
+def Hello():
+
+  user_ip = session.get('user_ip')
+  context ={
+    'user_ip':user_ip,
+    'todos': todos
+  }
+  print(context)
+  return render_template('hello.html', **context)
+
+
+
+if __name__ == "__main__":
+  app.run(debug=True)
