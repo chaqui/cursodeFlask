d25257d75124601aa2d97f66619032455527af04
Josue Chaqui <josue.chaqui@gmail.com>
2019-07-02 00:09:11 -0600

    agregando flask form

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from flask import Flask,request, make_response, redirect, render_template,session
 from flask_bootstrap import Bootstrap
-
-
+from flask_wtf import FlaskForm
+from wtforms.fields import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 
@@ -9,6 +10,12 @@ bootstrap = Bootstrap(app)
 
 app.config['SECRET_KEY'] = 'SUPER SECRETO'
 todos = ['todo 1', 'todo2', 'todo3']
+
+
+class LoginForm(FlaskForm):
+  username = StringField("Nombre de Usuario", validators=[DataRequired])
+  password = PasswordField("Password", validators=[DataRequired])
+  submit = SubmitField("Enviar")
 
 @app.errorhandler(404)
 def not_found(err):
@@ -28,7 +35,8 @@ def Hello():
   user_ip = session.get('user_ip')
   context ={
     'user_ip':user_ip,
-    'todos': todos
+    'todos': todos,
+    'login': LoginForm()
   }
   print(context)
   return render_template('hello.html', **context)
